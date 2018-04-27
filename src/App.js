c0b06d959651c6648c4ee3a28e845154daca30c9
王田 <kingwang@wangtiandeMacBook-Pro.local>
2018-04-27 11:20:25 +0800

    相册查看模态框完成

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Link,
     NavLink,
     Redirect
 } from 'react-router-dom';
@@ -51,6 +50,7 @@ class App extends Component {
                       </ul>
                   </div>
                   <div className={`content`}>
+                      {/*路由重定向*/}
                       <Route path={`/`} exact render={()=>(<Redirect to={`/Create`}/>)}/>
                       {
                           routes.map((route,i) => (
@@ -70,22 +70,17 @@ class Create extends Component{
         super(props);
         this.state={
             top:0,
-            title:'个人简历',
+            title:'',
             currentElem:0//当前选中的按钮
         }
     }
     componentDidMount(){
+        console.log(this.props.location)
 
     }
-    handleScroll=(e)=>{
-        e.preventDefault();
-        let position = parseInt(e.target.getAttribute('href')),
-            title = e.target.getAttribute('title');
-        this.setState({
-            top:-445*position,
-            currentElem:position,
-            title:title
-        })
+    handleClick=(e)=>{
+        const title = e.target.getAttribute('label');
+        this.setState({title:title})
     }
     render(){
         return(
@@ -94,11 +89,10 @@ class Create extends Component{
                     <ScrollToTopOnMount/>
                     <div className={`left-link`}>
                         <ul className={`links`}>
-                            <li><NavLink to={`/Create/MyResume`} activeClassName={`current`}>My Resume</NavLink></li>
-                            <li className={`${this.state.currentElem === 0?'current':''}`}><a onClick={this.handleScroll} title={`个人简历`} href="0">My Resume</a></li>
-                            <li className={`${this.state.currentElem === 1?'current':''}`}><a onClick={this.handleScroll} title={`成长履历`} href="1">Growth Record</a></li>
-                            <li className={`${this.state.currentElem === 2?'current':''}`}><a onClick={this.handleScroll} title={`一些照片`} href="2">Some Photo</a></li>
-                            <li className={`${this.state.currentElem === 3?'current':''}`}><a onClick={this.handleScroll} title={`一些爱好`} href="3">Some Hobby</a></li>
+                            <li><NavLink to={`/Create/MyResume`} onClick={this.handleClick} label={`个人简历`} activeClassName={`current`}>My Resume</NavLink></li>
+                            <li><NavLink to={`/Create/GrowthRecord`} onClick={this.handleClick} activeClassName={`current`} label={`个人履历`}>Growth Record</NavLink></li>
+                            <li><NavLink to={`/Create/MyPhotos`} onClick={this.handleClick} activeClassName={`current`} label={`一些照片`}>My Photos</NavLink></li>
+                            <li><NavLink to={`/Create/MyHobby`} onClick={this.handleClick} activeClassName={`current`} label={`个人喜好`}>My Hobby</NavLink></li>
                         </ul>
                     </div>
                     <div className={`right-content`}>
@@ -106,22 +100,7 @@ class Create extends Component{
                             <span className={`title`}>{this.state.title}</span>
                         </div>
                         <div className={`warp`}>
-                            {/*<ul className={`article-list`} ref={`article_list`} style={{top:this.state.top}}>*/}
-                               {/*<li className={`article`}>*/}
-                                    {/*<h2>王 田</h2>*/}
-                                    {/*<h4 className={`mt10`}>邮箱：2281334050@qq.com</h4>*/}
-                                    {/*<h4 className={`mt5`}>毕业院校：上海电视大学，计算机应用与管理</h4>*/}
-                                    {/*<h4 className={`mt5`}>Github：<a target={`_blank`} className={`linkStyle`} href="https://github.com/2281334050">Tian King</a></h4>*/}
-                               {/*</li>*/}
-                               {/*<li className={`article`}>*/}
-                                    {/*2*/}
-                               {/*</li>*/}
-                               {/*<li className={`article`}>*/}
-                                    {/*3*/}
-                               {/*</li>*/}
-                               {/*<li className={`article`}>*/}
-                               {/*</li>*/}
-                            {/*</ul>*/}
+                            {/*二级路由重定向*/}
                             <Route path={`/Create`} exact render={()=>(<Redirect to={`/Create/MyResume`}/>)}/>
                             {
                                 this.props.routes.map(function (route,i) {
@@ -135,27 +114,66 @@ class Create extends Component{
         )
     }
 }
-    /*相片路由*/
-function PhotoRoute() {
-    return(
-        <div className={`photos`}>
-            {
-                ImageData.map(function (key) {
-                    return(
-                       <Link key={key.id} to={{pathname:`/photo/${key.id}`}} >
-                            <Image url={key.url}/>
-                           <p>{key.title}</p>
-                       </Link>
-                    )
-                })
-            }
-        </div>
-    )
+    /*相片页逻辑*/
+class  PhotoRoute extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            ModalUrl:'',/*图片路径*/
+            isShow:false,/*查看大图模态框显示与隐藏*/
+            ImgTitle:'',/*图片title*/
+            picKey:0/*当前查看的图片key*/
+        }
+    }
+    handleClick = (e)=>{
+        let key = e.target.getAttribute('data-key');
+        this.setState((prevState)=>({
+            isShow:!prevState.isShow,
+        }));
+        this.changePic(key);
+    }
+    changePic = (key)=>{ /*公共改变图片方法*/
+        console.log(key)
+        if(key !== null){
+            this.setState({
+                ModalUrl:`http://localhost:3000/${ImageData[parseInt(key)].url}`,
+                ImgTitle:ImageData[parseInt(key)].title,
+                picKey:parseInt(key)
+            })
+        }
+    }
+    turnPage = (e) =>{
+        let direction = parseInt(e.target.getAttribute('direction'));
+        let key = this.state.picKey;
+        if(direction>0){
+            this.changePic(key+1);
+        }else{
+            this.changePic(key-1);
+        }
+    }
+    render(){
+        return(
+            <div className={`photos`}>
+                {
+                    ImageData.map( (key)=> {
+                        return(
+                            <a  href={`javascript:;`} onClick={this.handleClick} key={key.id} >
+                                {/*父组件传值不允许穿key属性*/}
+                                <Image url={`http://localhost:3000/${key.url}`} k={key.id} styles={{h:100,w:100}}/>
+                                <p data-key={key.id}>{key.title}</p>
+                            </a>
+                        )
+                    })
+                }
+                <Modal url={this.state.ModalUrl} turnPage={this.turnPage} picKey={this.state.picKey} title={this.state.ImgTitle} isShow={this.state.isShow} ModelClose={this.handleClick}/>
+            </div>
+        )
+    }
 }
 
 function Image(props) {
     return(
-        <img src={`http://localhost:3000/${props.url}`} style={{height:100,width:100}}/>
+        <img  src={props.url} data-key={props.k} style={{height:props.styles.h,width:props.styles.w}}/>
     )
 }
 
@@ -216,21 +234,32 @@ function TopBar(props) {
         </div>
     )
 }
+/*简介页*/
 function MyResume() {
     return(
-        <div>MyResume</div>
+        <div className={`article`}>
+            <h2>王 田</h2>
+            <h4 className={`mt10`}>邮箱：2281334050@qq.com</h4>
+            <h4 className={`mt5`}>毕业院校：上海电视大学，计算机应用与管理</h4>
+            <h4 className={`mt5`}>Github：<a target={`_blank`} className={`linkStyle`} href="https://github.com/2281334050">Tian King</a></h4>
+        </div>
     )
 }
+/*成长页*/
 function GrowthRecord() {
     return(
         <div>GrowthRecord</div>
     )
 }
+/*相册页*/
 function MyPhotos() {
     return(
-        <div>MyPhotos</div>
+        <div className={`article`}>
+            <PhotoRoute/>
+        </div>
     )
 }
+/*爱好页*/
 function MyHobby() {
     return(
         <div>MyHobby</div>
@@ -300,6 +329,41 @@ function Ajax_loading() {
         <div className={`ajaxLoader`}>
             <div className={`outer`}></div>
             <div className={`inner`}></div>
+        </div>
+    )
+}
+/*相册页查看大图模态框*/
+function Modal (props) {
+    if(!props.isShow){
+        return null
+    }
+    return (
+        <div className={`modal`}>
+            <div className={`pics`}>
+                <Image url={props.url} styles={{h:500,w:500}} />
+                <p>{props.title}</p>
+                <a className={`close icon-close`} href={`javascript:;`} onClick={props.ModelClose}>
+                </a>
+                {
+                    function () {
+                        if(props.picKey>0 && props.picKey < ImageData.length-1){
+                            return(
+                                <div>
+                                    <a href={`javascript:`} direction={-1} onClick={props.turnPage} className={`left-tick  icon-left-arrow`}></a>
+                                    <a href={`javascript:`} direction={1} onClick={props.turnPage} className={`right-tick  icon-left-arrow`}></a>
+                                </div>
+                                )
+
+                        }else{
+                            if(props.picKey === 0){
+                               return <a href={`javascript:;`} direction={1} onClick={props.turnPage} className={`right-tick  icon-left-arrow`}></a>
+                            }else if(props.picKey === ImageData.length-1){
+                                return <a href={`javascript:;`} direction={-1} onClick={props.turnPage} className={`left-tick  icon-left-arrow`}></a>
+                            }
+                        }
+                    }()
+                }
+            </div>
         </div>
     )
 }
