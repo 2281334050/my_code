6387f3adfb11396339e732f0311e3ffa8b85e7e1
王田 <kingwang@wangtiandeMacBook-Pro.local>
2018-08-16 16:46:54 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -223,23 +223,9 @@ class PersonalProjects extends Component{
         super(props)
     }
     componentDidMount(){
-        let baseUrl='';
-        if (process.env.NODE_ENV === 'development') {
-            baseUrl = 'http://localhost:3000';
-        } else {
-            // baseUrl = 'http://localhost:3000';
-        }
-// export const baseUrl = 'http://localhost:3000';
-        let $axios = axios.create({
-            baseURL: baseUrl + '/v2/movie',
-            timeout: 10000,
-            responseType: 'json',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-            }
-        });
-        $axios({
+        axios({
             method:'get',
+            url:'http://47.100.213.47:8080/index.php'
         }).then((res)=>{
             console.log(res)
         })
