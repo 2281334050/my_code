21e21adc5373a5e1a73f686d27b2fa7630dd29f1
王田 <kingwang@wangtiandeMacBook-Pro.local>
2018-09-10 17:16:05 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -245,7 +245,7 @@ class PersonalProjects extends Component{
          }
      }
      login = async(param)=>{
-        const res = await http.get('/api/login.php',param);
+        const res = await http.get('/api/info/login',param);
         if(res.status===200){
             this.setState({
                 token:res.data
